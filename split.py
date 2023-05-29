4c8ced4086fa0e677b98ef2cb673e43bdcdf6b64
levuphucnguyen <96976083+levuphucnguyen@users.noreply.github.com>
2023-05-30 05:17:24 +0700

    Optimize performance and add Vietnamese

--- split.py
+++ split.py
@@ -1,42 +1,29 @@
 import os
+from collections import defaultdict
+import unicodedata
 
-# Ask user to input file name
+def process_file(input_file, delimiter):
+    output_files = defaultdict(list)
+
+    with open(input_file, "r", encoding='utf-8') as f:
+        current_output_file = None
+        for line in f:
+            if line.startswith(delimiter):
+                title = line[len(delimiter):].strip()
+                current_output_file = title + ".txt"
+                output_files[current_output_file] = []
+            elif current_output_file:
+                output_files[current_output_file].append(line.strip())
+
+    for output_file, content in output_files.items():
+        with open(output_file, "w", encoding='utf-8') as out:
+            final_content = "\n".join(unicodedata.normalize('NFD', line.strip()) for line in content)
+            out.write(final_content)
+
+    print("Press any key to continue...")
+    input("")
+
 input_file = input("Enter input file name: ")
-
-# Set the delimiter
 delimiter = input("Enter delimiter: ")
 
-# Open the input file
-with open(input_file, "r") as f:
-    # Initialize the title and output file variables
-    title = ""
-    output_file = None
-    # Loop through each line in the input file
-    for line in f:
-        # Check if the line starts with the delimiter
-        if line.startswith(delimiter):
-            # Get the title without the delimiter and whitespace
-            title = line[len(delimiter):].strip()
-            # Create the output file name
-            output_file = title + ".txt"
-            # Open the output file
-            with open(output_file, "w") as out:
-                pass  # Create empty file
-            # Print the file name of the output file
-            print(f"Created file: {output_file}")
-        elif output_file is not None:
-            # Write the line to the output file
-            with open(output_file, "a") as out:
-                out.write(line)
-
-# Remove delimiter lines from the output files
-for output_file in os.listdir():
-    if output_file.endswith(".txt"):
-        with open(output_file, "r+") as out:
-            lines = out.readlines()
-            out.seek(0, 0)
-            out.truncate()
-            for line in lines:
-                if not line.startswith(delimiter):
-                    out.write(line)
-
+process_file(input_file, delimiter)
